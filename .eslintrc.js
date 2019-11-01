d5d1e5ab51f3f8dfcbd55027a0cab63ef60a8013
tomon9086 <tomo.mouse.9086@gmail.com>
2019-11-01 16:56:11 +0900

    :+1:eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "react-app",
+        "airbnb"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [2, {
+            "extensions": [".js", ".jsx", "ts", "tsx"]
+        }],
+        "quotes": [1, "double"],
+        "semi": [2, "never"]
+    }
+};
